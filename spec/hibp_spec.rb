c66ee9ce92c80c8a9a1104b8f251706d111eefbb
warshavski <p.warshavski@gmail.com>
2019-07-22 13:27:37 +0300

    Project initialization

---
+++ spec/hibp_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hibp do
+  it 'has a version number' do
+    expect(Hibp::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
